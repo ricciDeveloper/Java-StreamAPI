9c0aa882a30900a94c46b6950185ceebcb13e953
Joao Victor <joaovictor.ricci1999@gmail.com>
2024-01-29 18:48:06 -0300

    desafio stream API java

---
+++ Functional Interface/ExercicioStreamApi/src/Desafio10.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Desafio10 {
+    /*- Desafio 10 - Agrupe os valores ímpares múltiplos de 3 ou de 5:
+
+Utilize a Stream API para agrupar os valores ímpares múltiplos de 3 ou de 5 e exiba o resultado no console. */
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        
+        Map<Boolean, List <Integer>> resultado = numeros.stream()
+        //seleciona os ímpares
+            .filter(n-> n % 2 != 0)
+            //filtra os multiplos de 3 e 5
+            .filter(n -> n % 3 ==0 ||  n % 5 ==0)
+            //usamos collect, com collector e o método grouping By, para separar os multiplos de 3 e 5 em true. E outros impares em false
+            .collect(Collectors.groupingBy(n -> n %3 == 0 ||n % 5 ==0 ));
+
+            //imprimindo os resultados
+            System.out.println("Valores múltiplos de 3 ou 5 agrupados:");
+            //multiplos de 3 e 5 agrupados
+            System.out.println("multiplos de 3 ou 5: " + resultado.get(true));
+            //Outros impares, resultará em null, pois o exercicio pede apenas multiplos 3 e 5
+            System.out.println("outros ímpares: "+ resultado.get(false));
+
+        }
+}
---
+++ Functional Interface/ExercicioStreamApi/src/Desafio11.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio11 {
+    /*- Desafio 11 - Encontre a soma dos quadrados de todos os números da lista:
+
+Utilizando a Stream API, encontre a soma dos quadrados de todos os números da lista e exiba o resultado no console. */
+    public static void main(String[] args) {
+        
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+        //soma dos quadrados é n1²+n2²+n3²+n4²...
+        int somaQuadrados = numeros.stream()
+            .mapToInt(n -> n*n)
+            .sum();
+        System.out.println("A soma dos quadrados do array é: " + somaQuadrados);
+
+    }
+}
